148046c67e07db32ce6e5c898473c9620dbb557a
Miron <miron.kiselev11@mail.ru>
2025-03-22 19:30:25 +0300

    add logers

--- bot/handlers/user_handlers.py
+++ bot/handlers/user_handlers.py
@@ -1,3 +1,4 @@
+import logging
 from aiogram import Router
 from aiogram.filters import CommandStart
 from aiogram.types import Message
@@ -7,6 +8,9 @@ from bot.lexicon.lexicon_ru import lexicon
 router = Router()
 logger = logging.getLogger(__name__)
 
+
 @router.message(CommandStart())
 async def start_command(message: Message):
+    name, id = message.from_user.full_name, message.from_user.id
     await message.answer(lexicon["start_command"])
+    logger.info(f"Пользователь {name}({id}) запустил бота")
