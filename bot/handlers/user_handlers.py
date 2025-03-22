ccc541c97de764a317d55a65d8209a5c6b46ad25
Miron <miron.kiselev11@mail.ru>
2025-03-22 19:02:07 +0300

    add user start handler

---
+++ bot/handlers/user_handlers.py
@@ -0,0 +1,11 @@
+from aiogram import Router
+from aiogram.filters import CommandStart
+from aiogram.types import Message
+
+from bot.lexicon.lexicon_ru import lexicon
+
+router = Router()
+
+@router.message(CommandStart())
+async def start_command(message: Message):
+    await message.answer(lexicon["start_command"])
