2fa6177cf8a4aabcb96137acd600b418d61eed9b
Miron <miron.kiselev11@mail.ru>
2025-03-22 21:46:45 +0300

    create request

---
+++ app/services/https_requests.py
@@ -0,0 +1,10 @@
+import requests
+
+
+async def get_exchange_rate(code: str) -> str:  # noqa
+    answer = requests.get("https://www.cbr-xml-daily.ru/daily_json.js")
+    if answer.status_code == 200:
+        answer = answer.json()
+        answer = answer["Valute"][code]["Value"]
+        return "На данный момент эта валюта составляет" + answer + "рублей"
+    return "Извините, вы ввели неправильный код"
