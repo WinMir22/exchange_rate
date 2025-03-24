19cf31555275915a5041caba542e3f99ccf3103b
Miron <miron.kiselev11@mail.ru>
2025-03-24 17:19:33 +0300

    optimization

--- app/bot/lexicon/lexicon_ru.py
+++ app/bot/lexicon/lexicon_ru.py
@@ -2,7 +2,7 @@ lexicon = {
     "start_command": "Привет! Я могу присылать вам актуальные курсы валют."
     "\nОзнакомиться с командами можно, написав /help.",
     "help_command": "/help - Вызов этого меню\n/exchange_rate - Узнать курс валюты"
-    "\n/cancel - Закончить ввод валют",
+    "\n/cancel - Отменить ввод валют",
     "exchange_rate_command": "Введите три буквы - обозначения валюты. Например EUR, USD",
     "exchange_rate_error": "Вы ввели не существующий код. Eсли хотите закончить, введите /cancel",
     "exchange_rate_answer_command": "Желаете узнать еще? Введите команду /exchange_rate",
