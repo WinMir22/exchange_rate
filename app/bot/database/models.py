3c0ced93379579fd348dc3cf2f0ac8159e7a0550
Miron <miron.kiselev11@mail.ru>
2025-03-31 16:19:52 +0300

    fix bug

--- app/bot/database/models.py
+++ app/bot/database/models.py
@@ -17,7 +17,9 @@ class UsersTable(Base):
     )
     full_name: Mapped[str | None]
     username: Mapped[str | None]
-    currencies: Mapped[List[str]] = mapped_column(server_default=rates)
+    currencies: Mapped[List[str]] = mapped_column(
+        server_default="{" + ",".join(rates) + "}"
+    )
     created_at: Mapped[datetime] = mapped_column(server_default=func.now())
     updated_at: Mapped[datetime] = mapped_column(
         server_default=func.now(), onupdate=func.now()
