ac2756aca9c9de00debd33c59555cebcb3715f29
Miron <miron.kiselev11@mail.ru>
2025-04-03 16:10:50 +0300

    delete unusable

--- app/bot/lexicon/lexicon_ru.py
+++ app/bot/lexicon/lexicon_ru.py
@@ -1,17 +1,9 @@
 lexicon = {
-    "start_command": "Привет! Я могу присылать вам актуальные курсы валют."
-    "\nОзнакомиться с командами можно, написав /help.",
-    "help_command": "/help - Вызов этого меню\n/exchange_rate - Узнать курс валюты"
-    "\n/cancel - Отменить ввод валют",
+    "start_command": "Привет! Я могу присылать вам актуальные курсы валют.",
     "exchange_rate_command": "Введите три буквы - обозначения валюты. Например EUR, USD",
-    "get_code_for_check_state": "{check} стоит {result} USD. Выберите кнопку ниже, если хотите перевести "
-    "в другую валюту.",
-    "exchange_rate_error": "Вы ввели не существующий код. Eсли хотите закончить, введите /cancel",
-    "exchange_rate_answer_command": "Эта валюта составляет {} {}",
-    "not_cancel_command": "Вам нечего отменять",
-    "cancel_command": "Вы отменили ввод валют",
-    "exchange_rate_but": "Узнать курс валюты",
-    "exchange_rate_but2": "Курс всех валют",
+    "get_code_for_check_state": "{check} стоит {result} USD. "
+    "Выберите кнопку ниже, если хотите перевести в другую валюту.",
     "one_rate_but": "Курс любой валюты",
+    "exchange_rate_but": "Узнать курс валюты",
     "back_to_main_menu": "Назад",
 }
