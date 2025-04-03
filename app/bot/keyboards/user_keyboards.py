37fc9de9cdc03680e9b49ca3cb864d1b95d9f870
Miron <miron.kiselev11@mail.ru>
2025-04-03 15:47:19 +0300

    delete unusable keyboards

--- app/bot/keyboards/user_keyboards.py
+++
@@ -1,5 +0,0 @@
-from app.bot.keyboards.create_kb_func import create_inline_kb
-
-more_rate_kb = create_inline_kb(1, "more_rate")
-
-check_rates_kb = create_inline_kb(1, check_rates="Посмотреть доступные валюты")
