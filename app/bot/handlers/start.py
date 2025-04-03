684ad0c3728137caae9528f64aa4a04c9e5faccb
Miron <miron.kiselev11@mail.ru>
2025-04-03 15:48:28 +0300

    delete unusable imports

--- app/bot/handlers/start.py
+++ app/bot/handlers/start.py
@@ -1,17 +1,11 @@
 import logging
-from aiogram import Router, F
-from aiogram.filters import CommandStart, Command, StateFilter
-from aiogram.fsm.context import FSMContext
-from aiogram.fsm.state import default_state
-from aiogram.types import Message, CallbackQuery
+
+from aiogram import Router
+from aiogram.filters import CommandStart
+from aiogram.types import Message
 from aiogram_dialog import DialogManager
 
 from app.bot.dialogs.dialogs_states import StartSG
-from app.bot.keyboards.user_keyboards import more_rate_kb, check_rates_kb
-from app.bot.lexicon.lexicon_ru import lexicon
-from app.bot.states.states import FSMExchangeRate
-from app.services.exchange_rate_services.https_requests import get_exchange_rate
-from app.services.exchange_rate_services.list_of_rates import rates
 
 router = Router()
 logger = logging.getLogger(__name__)
